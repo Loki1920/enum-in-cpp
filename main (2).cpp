b1ebcad6a2f5c519981bf23798a8cd1d2afd71a0
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-04-15 22:42:15 +0530

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,14 @@
+//enum in c++
+#include<iostream>
+using namespace std;
+int main()
+{
+  enum Meal{breakfast,lunch,dinner};
+  Meal m1 = breakfast;
+  cout<<m1<<endl;
+  Meal m2 = lunch;
+  cout<<m2<<endl;
+  Meal m3 = dinner;
+  cout<<m3;
+  
+}
